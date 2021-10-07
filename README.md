1cc765bfe810ef1833ab614322ec35168ce6eb43
Your Name <you@example.com>
2021-10-06 21:09:06 -0300

    testeaula

---
+++ README.md
@@ -0,0 +1,2 @@
+# construdelasatividade
+# construdelasatividade
---
+++ arquivo.js
@@ -0,0 +1,65 @@
+aluno1 = prompt("Qual o nome do aluno 1?")
+nota1 = prompt("Qual as notas do aluno 1? Separe por vírgulas")
+nota1 = nota1.split(",")
+a1n1 = parseFloat(nota1[0])
+a1n2 = parseFloat(nota1[1])
+a1n3 = parseFloat(nota1[2])
+média1 = (a1n1 + a1n2 + a1n3) /3
+if(média1 >= 5) {
+    alert ("O aluno " + aluno1 + " teve média " + média1 + " e está APROVADO!")
+}
+else if(média1 <=5) {
+    alert ("O aluno " + aluno1 + " teve média " +  média1 + " e  está REPROVADO!")
+}
+aluno2 = prompt("Qual o nome do aluno 2?")
+nota2 = prompt("Qual as notas do aluno 2? Separe por vírgulas")
+nota2 = nota2.split(",")
+a2n1 = parseFloat(nota2[0])
+a2n2 = parseFloat(nota2[1])
+a2n3 = parseFloat(nota2[2])
+média2 = (a2n1 + a2n2 + a2n3) /3
+if(média2 >= 5) {
+    alert ("O aluno " + aluno2 + " teve média " +  média2 + " e está APROVADO!")
+}
+else if(média2 <=5) {
+    alert ("O aluno " + aluno2 + " teve média " +  média2 + " e está REPROVADO!")
+}
+aluno3 = prompt("Qual o nome do aluno 3?")
+nota3= prompt("Qual as notas do aluno 3? Separe por vírgulas")
+nota3 = nota3.split(",")
+a3n1 = parseFloat(nota3[0])
+a3n2 = parseFloat(nota3[1])
+a3n3 = parseFloat(nota3[2])
+média3 = (a3n1 + a3n2 + a3n3) /3
+if(média3 >= 5) {
+    alert ("O aluno " + aluno3 + " teve média " + média3 + " e está APROVADO!")
+}
+else if(média1 <=5) {
+    alert ("O aluno " + aluno3 + " teve média " + média3 + " e está REPROVADO!")
+}
+aluno4= prompt("Qual o nome do aluno 4?")
+nota4 = prompt("Qual as notas do aluno 4? Separe por vírgulas")
+nota4 = nota4.split(",")
+a4n1 = parseFloat(nota4[0])
+a4n2 = parseFloat(nota4[1])
+a4n3 = parseFloat(nota4[2])
+média1 = (a4n1 + a4n2 + a4n3) /3
+if(média4 >= 5) {
+    alert ("O aluno " + aluno4 + " teve média " + média4 + " e está APROVADO!")
+}
+else if(média4 <=5) {
+    alert ("O aluno " + aluno4 + " teve média " + média4 + " e está REPROVADO!")
+}
+aluno5 = prompt("Qual o nome do aluno 5?")
+nota5 = prompt("Qual as notas do aluno 5? Separe por vírgulas")
+nota5 = nota5.split(",")
+a5n1 = parseFloat(nota5[0])
+a5n2 = parseFloat(nota5[1])
+a5n3 = parseFloat(nota5[2])
+média5 = (a5n1 + a5n2 + a5n3) /3
+if(média5 >= 5) {
+    alert ("O aluno " + aluno5 + " teve média " + média5 + " e está APROVADO!")
+}
+else if(média5 <=5) {
+    alert ("O aluno " + aluno5 + " teve média " + média5 + " e está REPROVADO!")
+}
